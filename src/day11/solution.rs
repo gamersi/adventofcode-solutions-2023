bc5637128a9f391d04d1851782ebf4f713a7f738
RECHBERGER Simon, 2AHIF <rechbers@edu.htl-villach.at>
2023-12-12 14:09:21 +0100

    fixed expand map

--- src/day11/solution.rs
+++ src/day11/solution.rs
@@ -1,47 +1,79 @@
+use std::collections::VecDeque;
+
 const INPUT: &str = include_str!("example.txt");
+
+struct Spot {
+    x: usize,
+    y: usize,
+    visited: bool,
+    galaxy: bool
+}
 
 pub fn part1() {
     let mut sum = 0;
-    let mut map: Vec<Vec<char>> = Vec::new();
+    let mut map: Vec<Vec<Spot>> = Vec::new();
     let mut num_galaxies = 0;
-    for line in INPUT.lines() {
+    for (i, line) in INPUT.lines().enumerate() {
         let mut row = Vec::new();
-        for c in line.chars() {
-            row.push(c);
+        for (j, c) in line.chars().enumerate() {
+            let mut galaxy: bool = false;
             if c == '#' {
+                galaxy = true;
                 num_galaxies += 1;
-            }
+            } 
+            row.push(Spot {
+                x: j,
+                y: i,
+                visited: false,
+                galaxy
+            });
         }
         map.push(row);
     }
-    map = expand_map(map);
-    for row in map {
-        for c in row {
-            print!("{}", c);
-        }
-        println!();
-    }
+    map = expand_map(map, 1);
+    print_galaxy(map);
+
 }
 
 pub fn part2() {}
 
-fn expand_map(map: Vec<Vec<char>>) -> Vec<Vec<char>> {
+fn expand_map(map: Vec<Vec<char>>, expansion: u8) -> Vec<Vec<char>> {
     let mut new_map = Vec::new();
     for row in &map {
         if row.iter().any(|&c| c == '#') {
             new_map.push(row.clone());
         } else {
             new_map.push(row.clone());
-            new_map.push(vec!['.'; row.len()]);
+            for i in 0..expansion {
+                new_map.push(vec!['.'; row.len()]);
+            }
         }
     }
+    let mut offset = 0;
     for col in 0..map[0].len() {
         if map.iter().all(|row| row[col] == '.') {
-            println!("Adding column {}", col);
+            offset += 1;
             for row in &mut new_map {
-                row.insert(col, '.');
+                for i in 0..expansion {
+                    row.insert(col+offset, '.');
+                }
             }
         }
     }
     new_map
+}
+
+fn find_path(map: Vec<Vec<char>>, start: Spot, target: Spot) {
+    let mut queue: VecDeque<Spot> = VecDeque::new();
+    queue.push_back(start);
+    
+}
+
+fn print_galaxy(map: Vec<Vec<char>>) {
+    for row in map {
+        for c in row {
+            print!("{}", c);
+        }
+        println!();
+    }
 }
